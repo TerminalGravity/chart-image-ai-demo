be42fa0b1204497a8a5595567dbd9de9ec069ed8
TerminalGravity <jrinnfelke@gmail.com>
2024-01-27 19:38:16 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+## Imports
+# OpenAI, LangChain, ... 
+
+
+## Init OpenAI and RAG Clients
+
+
+
+## Init multi-modal embeddings on the .png images in the folder 'chart_images' 
+
+
+
+## Init the embedding pipeline to ChromaDB vector store
+
+
+
+## Init the LLM=RAG Pipeline
+
+
+
+## Init the Gradio chatbot interface 
+# Run the chat service 
